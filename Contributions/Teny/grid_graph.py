8290e460a852b49c832255bb3bd9a5ec39e5d491
teny373 <zhangtianning961@gmail.com>
2024-10-10 16:24:11 -0700

    Grid Graph (Python)

--- Contributions/Teny/grid_graph.py
+++ Contributions/Teny/grid_graph.py
@@ -2,23 +2,12 @@ from pyllist import sllist # type: ignore
 import matplotlib.pyplot as plt
 
 class Gridgraph:
-    def __init__(self, rows, cols):
-        self.rows = rows
-        self.cols = cols
+    def __init__(self):
         self.graph = {}
-        
-
-        for row in range(rows):
-            for col in range(cols):
-                node = (row,col)
-                self.graph[node] = sllist()
+    
+    def add_node(self, node):
+        self.graph[node] = sllist()
                 
-
-                if row > 0:
-                   self.add_edge(node,(row-1, col))
-                if col > 0:
-                  self.add_edge(node,(row, col-1))
-
     def add_edge(self, from_node, to_node):
         self.graph[from_node].append(to_node)
        
@@ -29,31 +18,44 @@ class Gridgraph:
             for b in graph1.graph:
                 product_graph.add_node((a,b))
 
-        for a in self.garaph:
+        for a in self.graph:
             for b in graph1.graph:
                 for neighbor in self.graph[a]:
                     product_graph.add_edge((a,b),(neighbor,b))
                 for neighbor in graph1.graph[b]:
                     product_graph.add_edge((a,b),(a,neighbor))
         return product_graph   
+
+def path_graph(n):
+    path = Gridgraph()
+    for i in range(n):
+        path.add_node(i)
+        if i >0:
+            path.add_edge(i,i-1)
+            path.add_edge(i-1,i)
+    return path 
     
-    def visualize(self):
-        pos = {(row,col):(col,-row) for row,col in self.graph.keys()}
-        plt.figure(figsize=(10,10))
+def visualize(self):
+    pos = {node:(node[1],-node[0]) for node in self.graph.keys()}
+    plt.figure(figsize=(10,10))
 
-        for node in self.graph:
-            for nearnode in self.graph[node]:
-                x = [pos[node][0],pos[nearnode][0]]
-                y = [pos[node][1],pos[nearnode][1]]
-                plt.plot(x,y,color='blue', linewidth=1 )
+    for node in self.graph:
+        for nearnode in self.graph[node]:
+            x = [pos[node][0],pos[nearnode][0]]
+            y = [pos[node][1],pos[nearnode][1]]
+            plt.plot(x,y,color='blue', linewidth=1 )
 
-        for node in self.graph:
-            plt.scatter(pos[node][0],pos[node][1],color='lightgreen')
-            plt.text(pos[node][0],pos[node][1],str(node),fontsize=10,ha='center',va='center')
+    for node in self.graph:
+        plt.scatter(pos[node][0],pos[node][1],color='lightgreen')
+        plt.text(pos[node][0],pos[node][1],str(node),fontsize=10,ha='center',va='center')
 
-        plt.title("Grid Graph Visualization")
-        plt.axis('off')
-        plt.show()
+    plt.title("Grid Graph Visualization")
+    plt.axis('off')
+    plt.show()
 
-grid_graph=Gridgraph(5,9)
-grid_graph.visualize()
+m = 5
+n = 4
+P_m = path_graph(m)
+P_n = path_graph(n)
+grid_graph = P_m.cartestian_product(P_n)
+visualize(grid_graph)
