f2db5c07451a04c8caa4aed015e1bf0f5f985392
teny373 <zhangtianning961@gmail.com>
2024-10-13 16:00:08 -0700

    BOOK GRAPH(with explanation)

--- Contributions/Teny/book_graph.py
+++ Contributions/Teny/book_graph.py
@@ -1,28 +1,31 @@
 from pyllist import sllist
-
+ #use the sllist data structure of pyllist library
+ #Bookgraph to represent the structure of the graph
 class Bookgraph:
     def __init__(self):
         self.graph = {}
-
+#initializes an empty dictionary self.graph
     def add_node(self,node):
         self.graph[node] = sllist()
-
+#Adds a new node to the graph and initializes an empty single-linked list for that node
     def add_edge(self, from_node, to_node):
         self.graph[from_node].append(to_node)
-
+#store neighboring nodes of each node
     def cartesian_product(self, graph1):
         product_graph = Bookgraph()
-
+#compute the Cartesian product of the current graph and another graph, and return a new graph
         for a in self.graph:
             for b in graph1.graph:
                 product_graph.add_node((a,b))
-
+#Use two for loops to travel all nodes of graph and create new modes
         for a in self.graph:
             for b in graph1.graph:
                 for neighbor in self.graph[a]:
                     product_graph.add_edge((a,b),(neighbor,b))
+                    # find the node adjacent to a and create an edge between it and (a, b)
                 for neighbor in graph1.graph[b]:
                     product_graph.add_edge((a,b),(a,neighbor))
+                    #Find the node adjacent to b and create an edge between it and (a, b)
         return product_graph
     
     def star_graph(self, m):
@@ -30,24 +33,27 @@ class Bookgraph:
             self.add_node(i)
         for i in range (1,m+1):
              self.add_edge(0,i)
-
+#Define star graph, connect 0 and each node
     def path_graph(self):
         self.add_node(0)
         self.add_node(1)
         self.add_edge(0,1)
+#Define path graph, connect 0 and 1
 
 m=3
 star_graph = Bookgraph()
 star_graph.star_graph(m)
-
+#Create a star graph with four nodes, node 0 connected to peripheral nodes 1,2,3
 path_graph = Bookgraph()
 path_graph.path_graph()
-
+#Create a simple pathgraph
 B_m = star_graph.cartesian_product(path_graph)
-
+#Create the Cartesian product of a star graph and a path graph to get a book graph
 print("Nodes in B_m are:")
 print(B_m.graph.keys())
 print("\nEdges in B_m:")
 for node in B_m.graph:
     for neighbor in B_m.graph[node]:
         print(f"{node} -> {neighbor}")
+#Print all nodes of B_m, as well as edges
+
--- Contributions/Teny/grid_graph.py
+++ Contributions/Teny/grid_graph.py
@@ -1,6 +1,6 @@
 from pyllist import sllist # type: ignore
 import matplotlib.pyplot as plt
-
+# Import the pyplot module of the matplotlib library for visualizing graphics
 class Gridgraph:
     def __init__(self):
         self.graph = {}
@@ -34,9 +34,11 @@ def path_graph(n):
             path.add_edge(i,i-1)
             path.add_edge(i-1,i)
     return path 
-    
+    #Define a path graph function that generates a path graph with n nodes
+    #If the current node is not the first node, an edge with the previous node will added
 def visualize(self):
     pos = {node:(node[1],-node[0]) for node in self.graph.keys()}
+    #store the coordinates of each node
     plt.figure(figsize=(10,10))
 
     for node in self.graph:
@@ -44,11 +46,11 @@ def visualize(self):
             x = [pos[node][0],pos[nearnode][0]]
             y = [pos[node][1],pos[nearnode][1]]
             plt.plot(x,y,color='blue', linewidth=1 )
-
+#Traverse each node and adjacent nodes in the graph, drawing their edges
     for node in self.graph:
         plt.scatter(pos[node][0],pos[node][1],color='lightgreen')
         plt.text(pos[node][0],pos[node][1],str(node),fontsize=10,ha='center',va='center')
-
+# Draw a scatter plot for each node and label the coordinates of the nodes
     plt.title("Grid Graph Visualization")
     plt.axis('off')
     plt.show()
@@ -59,3 +61,5 @@ P_m = path_graph(m)
 P_n = path_graph(n)
 grid_graph = P_m.cartestian_product(P_n)
 visualize(grid_graph)
+#Define two path graphs
+#Define a path graph
